2a1b912afb2264f75937d59fa2de1d7c876cd827
SimenScheers <86122823+SimenScheers@users.noreply.github.com>
2021-06-23 12:21:59 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,4 @@
+$(window).on('load', function(){ //makes sure whole website is loaded
+	$('#status').delay(2500).fadeOut();
+	$('#preloader').delay(2750).fadeOut();
+});
---
+++ js/script.js
@@ -0,0 +1,60 @@
+$(window).on('load',function(){ //makes sure whole website is loaded
+	$('#status').fadeOut(10000);
+	$('#preloader').delay(2850).fadeOut();
+});
+
+/* =========================================
+               Portfolio
+============================================ */
+$(window).on('load', function () {
+
+    // Initialize Isotope
+    $("#isotope-container").isotope({});
+
+    // filter items on button click
+    $("#isotope-filters").on('click', 'button', function () {
+
+        // get filter value
+        var filterValue = $(this).attr('data-filter');
+
+        // filter portfolio
+        $("#isotope-container").isotope({
+            filter: filterValue
+        });
+
+        // active button
+        $("#isotope-filters").find('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+
+/* =========================================
+               Magnifier
+============================================ */
+$(function () {
+
+    $("#portfolio-wrapper").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+});
+
+/* =========================================
+               Testimonials
+============================================ */
+$(function () {
+    $("#testimonial-slider").owlCarousel({
+        items: 1,
+        autoplay: false,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    });
+});
